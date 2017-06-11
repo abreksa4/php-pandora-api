fa1f46365b79167625b5e02aa1a0c24d49b55013
Andrew Breksa <abreksa4@gmail.com>
2017-06-11 15:29:11 -0700

    remove unneeded constructor parameter

--- examples/getStationList.php
+++ examples/getStationList.php
@@ -4,7 +4,7 @@ require_once '../src/php_pandora_api/Pandora.php';
 
 use php_pandora_api\Pandora;
 
-$p = new Pandora('android', 'json');
+$p = new Pandora('android');
 
 if (!$p->login('jonathon@compwright.com', 'cleanup')) {
     die(sprintf("Error: %s\nReq: %s\n Resp: %s", $p->last_error, $p->last_request_data, $p->last_response_data));
